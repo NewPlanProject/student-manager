121096de5e61ead411c780e299c07f49e9c395b1
jianan <15801316163@163.com>
2018-06-04 14:07:03 +0800

    更改新的服务器信息

---
+++ src/main/java/org/heran/edu/student/web/ProcessImgController.java
@@ -0,0 +1,36 @@
+package org.heran.edu.student.web;
+
+import com.alibaba.fastjson.JSON;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.heran.edu.student.service.StuInfoService;
+import org.heran.edu.student.util.data.Result;
+import org.heran.edu.student.util.data.ResultCode;
+import org.heran.edu.student.util.fdfs.FastDFSClientWrapper;
+import org.heran.edu.student.vo.StuInfoInVO;
+import org.heran.edu.student.vo.StudentRegisterInVO;
+import org.heran.edu.student.vo.StudentUpdateInVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Slf4j
+@Controller
+@RequestMapping("/img")
+public class ProcessImgController {
+
+
+    @ResponseBody
+    @RequestMapping(value = "/imageStr", method = RequestMethod.POST)
+    public String imageStr(){
+        return null;
+    }
+
+}
