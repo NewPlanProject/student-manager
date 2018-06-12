199a178aabd8514c8536a66eaffb484ea7893d3b
jianan <15801316163@163.com>
2018-06-12 18:49:49 +0800

    新增返回类

--- src/main/java/org/heran/edu/student/vo/GradeSaveInVO.java
+++ src/main/java/org/heran/edu/student/vo/GradeSaveInVO.java
@@ -32,7 +32,7 @@ public class GradeSaveInVO{
     @ApiModelProperty(value = "成绩",example = "成绩")
     private String mark;
     @ApiModelProperty(value = "考试时间",example = "考试时间")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM")
     private Date examTime;
 
 }
